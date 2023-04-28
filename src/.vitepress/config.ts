9330ef87aa0e9b1dd1bc8faf5e9095d43fb43b9a
John Campion Jr <john@brightshore.com>
2023-04-28 19:17:36 -0400

    fix: various

--- src/.vitepress/config.ts
+++ src/.vitepress/config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
       { text: 'Home', link: '/' },
       { text: 'Drivers', link: '/reference/drivers/2023' },
       { text: 'Utilities', link: '/reference/utilities' },
-      { text: 'Wallpapers', link: '/reference/wallpapers' },
+      { text: 'Wallpapers', link: '/wallpapers' },
       {
         text: 'Blog',
         items: [
--- src/wallpapers/index.md
+++ src/wallpapers/index.md
@@ -1,3 +1,4 @@
 # Wallpapers
 
 [Windows 365](/images/wallpapers/windows365wallpaper.jpg)
+[Test Windows 365](./windows365wallpaper.jpg)
